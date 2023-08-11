555e786cd9a0bea5b3ab9eaffdf5edb13769e64c
oscar <oscaranaya75@gmail.com>
2023-08-11 14:49:48 -0700

    correccion de codigo en  el if anidado
    se realizaron modificacion en el   se  dio una variable para  la conversion de  mayusculas a minusculas  ya definidida  desde el principio

--- script/main.js
+++ script/main.js
@@ -17,10 +17,10 @@ si es de noche  y con clima frio  ponte una pants y sueter
 si es de noche y con clima caliente ponte un unos short 
 */
 
-//confirm( "este programa te ayudara a saber que look puedes vestir, sigue las instrucciones")
+confirm( "este programa te ayudara a saber que look puedes vestir, sigue las instrucciones")
 var tipoDesexo = prompt("Eres mujer o hombre? Por favor escribe 'M' si eres mujer y 'H' si eres hombre");
 var diaOnoche = prompt("Es de día o de noche? Escribe 'N' si es de noche y 'D' si es de día");
-var tipoDeclima = prompt("¿El clima es frío o caliente? Teclea 'F' para frío y 'C' para caliente");
+var tipoDeclima = prompt("¿El clima es frío o caliente? Teclea 'F' para frío y 'C' para calor");
 
 tipoDesexo = tipoDesexo.toLowerCase(); // Convertir a minúsculas para hacer comparaciones más fáciles
 diaOnoche = diaOnoche.toLowerCase();
@@ -29,7 +29,7 @@ var imageTag ="<img src='%s' style='display: block; margin: 0 auto; max-width: 1
 if (tipoDesexo === "h" || tipoDesexo === "hombre") {
     if (diaOnoche === "d" || diaOnoche === "dia") {
         if (tipoDeclima === "f"|| tipoDeclima === "frio") {
-            console.log("Ponte traje con saco");
+            console.log("Ponte traje con saco");h
             document.write(imageTag.replace('%s', './img/hdf.jpeg'));
         } else if (tipoDeclima === "c" || tipoDeclima === "calor") {
             console.log("Ponte short con playera");
@@ -56,7 +56,7 @@ if (tipoDesexo === "h" || tipoDesexo === "hombre") {
     } else if (diaOnoche === "n" || diaOnoche === "noche") {
         if (tipoDeclima === "f" || tipoDeclima === "frio") {
            
-            document.write("<img src='./img/mnf.jpeg' style='display: block; margin: 0 auto; max-width: 100%; height: auto; margin-top: 20px;'></img>");
+            document.write("<img src='./img/mnf.jpeg'  style='display: block; margin: 0 auto; max-width: 100%; height: auto; margin-top: 20px; background-color: #0f0e0e;'></img>");
         } else if (tipoDeclima === "c" || tipoDeclima === "caliente") {
             
             document.write("<img src='./img/mdc.jpeg' style='display: block; margin: 0 auto; max-width: 100%; height: auto; margin-top: 20px;'></img>");
