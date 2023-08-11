5453e478855b79f0bda9a538efd1af46b0cd4e92
Oscar Anaya <oscaranaya75@gmail.com>
2023-08-10 23:15:21 -0700

    subir  imagenes

--- script/main.js
+++ script/main.js
@@ -18,23 +18,41 @@ si es de noche y con clima caliente ponte un unos short
 */
 
 //alert( "este programa te ayudara a saber que te pones")
-var tipoDesexo = prompt( "eres  mujer o hombre, por favor escribe M(si eres mujer) y H ( si eres hombre")
+var tipoDesexo = prompt("eres  mujer o hombre, por favor escribe M(si eres mujer) y H ( si eres hombre")
 
 
-var diaOnoche = prompt ("es de dia o de noche: Escribre N(si es de noche) y D(si es de dia)")
+var diaOnoche = prompt("es de dia o de noche: Escribre N(si es de noche) y D(si es de dia)")
 
-var tipoDeclima = prompt(" tipo de clima es frio o caliente")
+var tipoDeclima = prompt(" tipo de clima es frio o caliente: Teclea F para frio y C para caliente")
 
 
-if (tipoDesexo.toLowerCase() == "h" && diaOnoche.toLowerCase() == "d" && tipoDeclima.toLowerCase() == "c"){
-    /* if (diaOnoche.toLowerCase() == "d"){
-        if (tipoDeclima.toLowerCase() == "c"){
+if (tipoDesexo.toLowerCase() == "h") {
+    if (diaOnoche.toLowerCase() == "d") {
+        if (tipoDeclima.toLowerCase() == "f") {
+            console.log("ponte traje con saco")
+        } else if (tipoDeclima.toLowerCase() == "c")
+            console.log("ponte short con playera")
 
+    } else if (diaOnoche.toLowerCase() == "n") {
+        if (tipoDeclima.toLowerCase() == "f") {
+            console.log("ponte un abrigo")
+        } else (tipoDeclima.toLowerCase() == "c")
+            console.log("ponte un  short con tank")
         }
-        /* ponte un saco de dia */
-        console.log("ponte pantalon")
-    
-  
 
-}else (tipoDesexo.toLowerCase == "m")
-    console.log ("ponte vestido")
+
+} else (tipoDesexo.toLowerCase == "m");{
+   if (diaOnoche.toLowerCase() == "d") {
+        if (tipoDeclima.toLowerCase() == "f") {
+            console.log("ponte vestido con saco")
+        } else if (tipoDeclima.toLowerCase() == "c")
+            console.log("ponte un vestido  fresco")
+   
+        }else if (diaOnoche.toLowerCase() == "n"){
+        if (tipoDeclima.toLowerCase() == "f"){
+            console.log("ponte una pants y sueter")
+        
+        } else (tipoDeclima.toLowerCase() == "c")
+            console.log("ponte un unos short ")
+    }
+}
